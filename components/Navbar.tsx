1de2019a34b3d79316a4bbe6d7608d6c2d9e9d13
jonathanopigez <101323837+jonathanopigez@users.noreply.github.com>
2023-02-18 14:19:21 +0100

    swipeExperienceCard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,13 +10,17 @@ function Navbar({}: Props) {
   return (
     <div className="fixed bottom-0  transform -translate-x-1/2 pb-2 left-1/2 flex flex-col z-40 ">
     <button
-      className="bg-gray-700 text-white font-medium uppercase p-2 "
+      className="bg-gray-700 text-white font-medium uppercase p-2 justify-eveny "
       onClick={() => setIsMenuOpen(!isMenuOpen)}
     >
       Menu
     </button>
     {isMenuOpen && (
+        
       <div className="bg-gray-700 flex flex-col  font-medium uppercase p-2 w-screen mt-0 z-40 h-52">
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)}
+         className='absolute top-1 right-4 p-1 text-2xl profilButton'>X</button>
+        <Link href="#home"><button className='profilButton hover:text-[#6E61Ca]'>Acceuil</button></Link>
     <Link href="#about"><button className='profilButton hover:text-[#6E61Ca]'>à propos</button></Link>
         <Link href="#experiences"><button className='profilButton hover:text-[#6E61Ca]'>Expériences</button></Link>
         <Link href="#competences"><button className='profilButton hover:text-[#6E61Ca]'>Compétences</button></Link>
